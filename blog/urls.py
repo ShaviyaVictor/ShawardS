08884b40edaf320afb30ed790a0afab985805626
ShaviyaVictor <shaviyavictor@gmail.com>
2022-03-12 13:38:19 +0300

    Style the body and give a background color

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='blog-home'),
-    path('collection/', views.collection, name='blog-collections'),
+    path('collection/', views.collection, name='blog-collection'),
     path('talent/', views.talent, name='blog-talent'),
     path('blog/', views.blog, name='blog-blog'),
     path('about/', views.about, name='blog-about'),
