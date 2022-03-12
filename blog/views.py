986b6f729e9a868dceda40b9fbbcdbdf04d4d530
ShaviyaVictor <shaviyavictor@gmail.com>
2022-03-12 08:59:15 +0300

    Add the blog to the list of installed apps in the settings

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request) :
 
-  return HttpResponse('<h1>Blog Home</h1>')
+  return HttpResponse()
 
 
 
--- shawards_project/settings.py
+++ shawards_project/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
