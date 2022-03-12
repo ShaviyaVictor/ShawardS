b7706b3d580a692b07bbb6e3efe1bb4117e1964f
ShaviyaVictor <shaviyavictor@gmail.com>
2022-03-12 21:10:01 +0300

    Install dependancies and create the Post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
+from operator import mod
+from tkinter import CASCADE
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+
 
 # Create your models here.
+class Post(models.Model) :
+  project_name = models.CharField(max_length=50)
+  date_posted = models.DateTimeField(default=timezone.now)
+  author = models.ForeignKey(User, on_delete=models.CASCADE)
+  image = models.ImageField(upload_to='posts/')
+
+  def __str__(self) :
+    return self.project_name
+
+  class Meta :
+    ordering = ['-date_posted']
