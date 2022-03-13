9f98d0cb9b2dac4cf9a6880c4f9505aa42c05618
ShaviyaVictor <shaviyavictor@gmail.com>
2022-03-13 19:38:25 +0300

    Redirect to login after registration

--- users/views.py
+++ users/views.py
@@ -14,9 +14,9 @@ def register(request) :
     if form.is_valid() :
       form.save()
       username = form.cleaned_data.get('username')
-      messages.success(request, f'Account created for { username }.')
+      messages.success(request, f'Account created for { username }. Login to continue')
 
-      return redirect('blog-home')
+      return redirect('login')
 
   else :
     form = UserRegisterForm()
