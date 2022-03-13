3ded18a45458275160885ca08c3722f62d12a2e5
ShaviyaVictor <shaviyavictor@gmail.com>
2022-03-13 17:03:00 +0300

    Form validation conditional

--- users/views.py
+++ users/views.py
@@ -4,11 +4,15 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 
+
 # Create your views here.
 def register(request) :
 
   if request.method == 'POST' :
     form = UserCreationForm(request.POST)
+
+    if form.is_valid() :
+      username = form.cleaned_data.get('username')
 
   else :
     form = UserCreationForm()
