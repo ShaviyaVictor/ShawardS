88ced2c643fb7259d7575ca34dc75a29148ba820
ShaviyaVictor <shaviyavictor@gmail.com>
2022-03-13 17:18:37 +0300

    Save the form details

--- users/views.py
+++ users/views.py
@@ -12,9 +12,10 @@ def register(request) :
     form = UserCreationForm(request.POST)
 
     if form.is_valid() :
+      form.save()
       username = form.cleaned_data.get('username')
       messages.success(request, f'Account created for { username }.')
-      
+
       return redirect('blog-home')
 
   else :
