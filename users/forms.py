9cdcc0a4ebd86aac017d90dd0fe626b8be0cb797
ShaviyaVictor <shaviyavictor@gmail.com>
2022-03-13 17:32:24 +0300

    Create a form instance with additional fields of the form

---
+++ users/forms.py
@@ -0,0 +1,19 @@
+from pyexpat import model
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+class UserRegisterForm(UserCreationForm) :
+  email = forms.EmailField()
+
+  class Meta :
+    model = User
+    fields = [
+      'username',
+      'first_name',
+      'last_name',
+      'email',
+      'password1',
+      'password2',
+    ]
