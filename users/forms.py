2a925adf89495229415686b1f0de8e3e7d7bdef1
ShaviyaVictor <shaviyavictor@gmail.com>
2022-03-14 07:23:13 +0300

    Create User Update and Profile Update forms for front end update of the profile

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,9 @@
+import email
 from pyexpat import model
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 
 class UserRegisterForm(UserCreationForm) :
@@ -16,4 +18,26 @@ class UserRegisterForm(UserCreationForm) :
       'email',
       'password1',
       'password2',
+    ]
+
+
+
+class UserUpdateForm(forms.ModelForm) :
+  email = forms.EmailField()
+
+  class Meta :
+    model = User
+    fields = [
+      'username',
+      'email',
+    ]
+
+
+
+class ProfileUpdateForm(forms.ModelForm) :
+
+  class Meta :
+    model = Profile
+    fields = [
+      'image'
     ]
