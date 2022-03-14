4596930a410f32d6c62dff983cafcac34b5c5faf
ShaviyaVictor <shaviyavictor@gmail.com>
2022-03-14 18:50:54 +0300

    Create a url path for a single post item listed

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from . import views
-from .views import PostListView
+from .views import PostListView, PostDetailView
 
 
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
+
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+
     path('collection/', views.collection, name='blog-collection'),
     path('talent/', views.talent, name='blog-talent'),
     path('blog/', views.blog, name='blog-blog'),
