a12eebd7d5397dfe8aa37cee2fdcaa87043eeaf0
ShaviyaVictor <shaviyavictor@gmail.com>
2022-03-14 08:02:43 +0300

    Check the request method being called and instantiate to the forms data of the current logged in user

--- users/views.py
+++ users/views.py
@@ -38,10 +38,16 @@ def profile(request) :
 @login_required
 def update_profile(request) :
 
+  if request.method == 'POST' :
 
+    u_form = UserUpdateForm(request.POST, instance=request.user)
+    p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
 
-  u_form = UserUpdateForm(instance=request.user)
-  p_form = ProfileUpdateForm(instance=request.user.profile)
+    
+
+  else :
+      u_form = UserUpdateForm(instance=request.user)
+      p_form = ProfileUpdateForm(instance=request.user.profile)
 
   context = {
     'u_form': u_form,
