6883279b5c2551d6eae4c84df254958ce5b3403c
ShaviyaVictor <shaviyavictor@gmail.com>
2022-03-14 19:56:44 +0300

    Use login mixin to redirect to login page in order to post

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
 from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 class PostListView(ListView) :
@@ -34,14 +35,14 @@ class PostDetailView(DetailView) :
 
 
 
-class PostCreateView(CreateView) :
+class PostCreateView(LoginRequiredMixin, CreateView) :
   model = Post
   fields = [
     'project_name',
     'image'
   ]
   
-  
+
   def form_valid(self, form) :
     form.instance.author = self.request.user    
     return super().form_valid(form)
