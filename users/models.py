59095606ad9ae502fe2d6997ee77c5ae9e5c0ad8
ShaviyaVictor <shaviyavictor@gmail.com>
2022-03-14 08:20:52 +0300

    Resize our images

--- users/models.py
+++ users/models.py
@@ -20,9 +20,9 @@ class Profile(models.Model) :
   def save(self) :
     super().save()
 
-    img = Image.open(self.image.path)
+    img = Image.open(self.profile_img.path)
 
     if img.height > 300 or img.width > 300 :
       output_size = (300, 300)
       img.thumbnail(output_size)
-      img.save(self.image.path)
+      img.save(self.profile_img.path)
