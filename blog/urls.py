787c437f845850e70bbc8c88ffc103f2e0f8d5bb
ShaviyaVictor <shaviyavictor@gmail.com>
2022-03-14 20:13:17 +0300

    Create the delete url path

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 
 
@@ -10,6 +10,7 @@ urlpatterns = [
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'),
 
     path('collection/', views.collection, name='blog-collection'),
     path('talent/', views.talent, name='blog-talent'),
