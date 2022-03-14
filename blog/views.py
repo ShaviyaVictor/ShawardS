4fc66199dce84f9b94de0b08a9202cff4ec7d4bc
ShaviyaVictor <shaviyavictor@gmail.com>
2022-03-14 20:12:15 +0300

    Create a Post Delete view class and a test func to ensure only the authorized parties can delete a post

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from re import template
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 # Create your views here.
@@ -60,6 +60,18 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView) :
   def form_valid(self, form) :
     form.instance.author = self.request.user    
     return super().form_valid(form)
+
+
+  def test_func(self) :
+    post = self.get_object()
+    if self.request.user == post.author :
+      return True  
+    return False
+
+
+# Post Delete View
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin,DeleteView) :
+  model = Post
 
 
   def test_func(self) :
