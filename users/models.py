4a78bd45f618d3a5cdcdbe4e0cee498b5dbecf91
ShaviyaVictor <shaviyavictor@gmail.com>
2022-03-14 08:17:37 +0300

    Overwrite the models save method and resize the images before saving

--- users/models.py
+++ users/models.py
@@ -1,6 +1,7 @@
 from email.policy import default
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 
 
@@ -16,3 +17,12 @@ class Profile(models.Model) :
     return f'{self.user.username}\'s Profile'
 
   
+  def save(self) :
+    super().save()
+
+    img = Image.open(self.image.path)
+
+    if img.height > 300 or img.width > 300 :
+      output_size = (300, 300)
+      img.thumbnail(output_size)
+      img.save(self.image.path)
