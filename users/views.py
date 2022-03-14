e41abc75c6ab5ce7722ccba5cdefc93ecabad909
ShaviyaVictor <shaviyavictor@gmail.com>
2022-03-14 08:03:14 +0300

    Check if the form data is valid and save

--- users/views.py
+++ users/views.py
@@ -43,7 +43,9 @@ def update_profile(request) :
     u_form = UserUpdateForm(request.POST, instance=request.user)
     p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
 
-    
+    if u_form.is_valid() and p_form.is_valid() :
+      u_form.save()
+      p_form.save()
 
   else :
       u_form = UserUpdateForm(instance=request.user)
