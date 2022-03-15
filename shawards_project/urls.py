823a4394ecbf5f9f509af7b6647959a1e0f6cbcb
ShaviyaVictor <shaviyavictor@gmail.com>
2022-03-15 16:18:46 +0300

    Create a url route for password reset confirmation

--- shawards_project/urls.py
+++ shawards_project/urls.py
@@ -34,10 +34,11 @@ urlpatterns = [
 
     path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password-reset-done'),
 
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password-reset-confirm'),
+
     path('', include('blog.urls')),
 
 ]
-
 
 
 if settings.DEBUG :
