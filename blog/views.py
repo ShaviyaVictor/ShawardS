70a825b9f175486549c397764afd872faa30d063
ShaviyaVictor <shaviyavictor@gmail.com>
2022-03-15 14:33:16 +0300

    Adjust the user view section page and change function to queryset without underscore

--- blog/views.py
+++ blog/views.py
@@ -36,7 +36,7 @@ class UserPostListView(ListView) :
 
   paginate_by = 8
 
-  def get_query_set(self) :
+  def get_queryset(self) :
     user = get_object_or_404(User, username=self.kwargs.get('username'))
     return Post.objects.filter(author=user).order_by('-date_posted')
 
