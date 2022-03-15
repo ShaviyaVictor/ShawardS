6a29969745504a629f60d6155c9478cfaede9977
ShaviyaVictor <shaviyavictor@gmail.com>
2022-03-15 16:08:32 +0300

    Create a password reset done notification template

--- shawards_project/urls.py
+++ shawards_project/urls.py
@@ -29,7 +29,11 @@ urlpatterns = [
     path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+
     path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password-reset'),
+
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password-reset-done'),
+
     path('', include('blog.urls')),
 
 ]
