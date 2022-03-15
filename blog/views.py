16073dcd4d283a2b292a18e4a37a2b4eaad98c43
ShaviyaVictor <shaviyavictor@gmail.com>
2022-03-15 14:04:37 +0300

    Filter through the return values for the UserPostListView class view for getting the posts of a specific user

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,11 @@
 from dataclasses import fields
 from re import template
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
 
 # Create your views here.
 # def home function replacement
@@ -31,9 +32,13 @@ class UserPostListView(ListView) :
 
   context_object_name = 'posts'
 
-  ordering = ['-date_posted']
+  # ordering = ['-date_posted']
 
   paginate_by = 8
+
+  def get_query_set(self) :
+    user = get_object_or_404(User, username=self.kwargs.get('username'))
+    return Post.objects.filter(author=user).order_by('-date_posted')
 
 
 
