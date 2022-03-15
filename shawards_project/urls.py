56d942ec952705bfa2b9f911210dc82fbb939e2b
ShaviyaVictor <shaviyavictor@gmail.com>
2022-03-15 16:03:42 +0300

    Create a URL for password reset

--- shawards_project/urls.py
+++ shawards_project/urls.py
@@ -29,7 +29,9 @@ urlpatterns = [
     path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password-reset'),
     path('', include('blog.urls')),
+
 ]
 
 
