e335193abad85fabfa64f782a4466eec3c84bf99
ShaviyaVictor <shaviyavictor@gmail.com>
2022-03-16 09:41:42 +0300

    Add args and kwargs to the super save method to allow us save an instanche of a newly registering user

--- users/models.py
+++ users/models.py
@@ -17,8 +17,8 @@ class Profile(models.Model) :
     return f'{self.user.username}\'s Profile'
 
   
-  def save(self) :
-    super().save()
+  def save(self, *args, **kwargs) :
+    super().save(*args, **kwargs)
 
     img = Image.open(self.profile_img.path)
 
